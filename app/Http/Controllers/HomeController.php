1b031e87b7a4b6adb46c204ac290b65a25784ac6
osama-zayed <o.a.s775561590@gmail.com>
2024-06-19 04:26:17 +0300

    New features

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,35 +11,35 @@ class HomeController extends Controller
      * Display a listing of the resource.
      */
     public function index()
-{
-    $perPage = request()->get('per_page', 9); // Set the number of products per page
-    $page = request()->get('page', 1); // Get the current page from the request
+    {
+        // $perPage = request()->get('per_page', 9); // Set the number of products per page
+        // $page = request()->get('page', 1); // Get the current page from the request
 
-    $products = Product::select(
-        'id',
-        'product_name',
-        'product_price',
-        'image',
-        'discount_percentage',
-        'category_id'
-    )
-    ->where('product_status', 1)
-    ->with('Category:id,category_name')
-    // ->orderByDesc('id')
-    ->paginate($perPage, ['*'], 'page', $page);
+        // $products = Product::select(
+        //     'id',
+        //     'product_name',
+        //     'product_price',
+        //     'image',
+        //     'discount_percentage',
+        //     'category_id'
+        // )
+        //     ->where('product_status', 1)
+        //     ->with('Category:id,category_name')
+        //     // ->orderByDesc('id')
+        //     ->paginate($perPage, ['*'], 'page', $page);
 
-    $groupedProducts = $products->groupBy(function ($product) {
-        return $product->Category->category_name;
-    });
+        // $groupedProducts = $products->groupBy(function ($product) {
+        //     return $product->Category->category_name;
+        // });
 
-    $groupedProducts = $groupedProducts->map(function ($products) {
-        return $products->take(3);
-    });
-// dd($groupedProducts);
-    return view('page.home', [
-        'groupedProducts' => $groupedProducts
-    ]);
-}
+        // $groupedProducts = $groupedProducts->map(function ($products) {
+        //     return $products->take(3);
+        // });
+        // dd($groupedProducts);
+        return view('page.home', [
+            // 'groupedProducts' => $groupedProducts
+        ]);
+    }
 
     /**
      * Show the form for creating a new resource.
--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -1,147 +1,93 @@
 @extends('layout.app')
 @section('content')
-    <div class="fashion_section">
-        <div id="main_slider" class="carousel slide" data-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <div class="container">
-                        <h1 class="fashion_taital">Man & Woman Fashion</h1>
-                        <div class="fashion_section_2">
-                            <div class="row">
-                                <div class="col-lg-4 col-sm-4">
-                                    <div class="box_main">
-                                        <h4 class="shirt_text">Man T -shirt</h4>
-                                        <p class="price_text">Price <span style="color: #262626;">$ 30</span></p>
-                                        <div class="tshirt_img"><img src="{{ asset('asset/images/tshirt-img.png') }}"></div>
-                                        <div class="btn_main">
-                                            <div class="buy_bt"><a href="#">Buy Now</a></div>
-                                            <div class="seemore_bt"><a href="#">See More</a></div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-4 col-sm-4">
-                                    <div class="box_main">
-                                        <h4 class="shirt_text">Man -shirt</h4>
-                                        <p class="price_text">Price <span style="color: #262626;">$ 30</span></p>
-                                        <div class="tshirt_img"><img src="{{ asset('asset/images/dress-shirt-img.png') }}">
-                                        </div>
-                                        <div class="btn_main">
-                                            <div class="buy_bt"><a href="#">Buy Now</a></div>
-                                            <div class="seemore_bt"><a href="#">See More</a></div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-4 col-sm-4">
-                                    <div class="box_main">
-                                        <h4 class="shirt_text">Woman Scart</h4>
-                                        <p class="price_text">Price <span style="color: #262626;">$ 30</span></p>
-                                        <div class="tshirt_img"><img
-                                                src="{{ asset('asset/images/women-clothes-img.png') }}"></div>
-                                        <div class="btn_main">
-                                            <div class="buy_bt"><a href="#">Buy Now</a></div>
-                                            <div class="seemore_bt"><a href="#">See More</a></div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="container">
-                        <h1 class="fashion_taital">Man & Woman Fashion</h1>
-                        <div class="fashion_section_2">
-                            <div class="row">
-                                <div class="col-lg-4 col-sm-4">
-                                    <div class="box_main">
-                                        <h4 class="shirt_text">Man T -shirt</h4>
-                                        <p class="price_text">Price <span style="color: #262626;">$ 30</span></p>
-                                        <div class="tshirt_img"><img src="{{ asset('asset/images/tshirt-img.png') }}"></div>
-                                        <div class="btn_main">
-                                            <div class="buy_bt"><a href="#">Buy Now</a></div>
-                                            <div class="seemore_bt"><a href="#">See More</a></div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-4 col-sm-4">
-                                    <div class="box_main">
-                                        <h4 class="shirt_text">Man -shirt</h4>
-                                        <p class="price_text">Price <span style="color: #262626;">$ 30</span></p>
-                                        <div class="tshirt_img"><img src="{{ asset('asset/images/dress-shirt-img.png') }}">
-                                        </div>
-                                        <div class="btn_main">
-                                            <div class="buy_bt"><a href="#">Buy Now</a></div>
-                                            <div class="seemore_bt"><a href="#">See More</a></div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-4 col-sm-4">
-                                    <div class="box_main">
-                                        <h4 class="shirt_text">Woman Scart</h4>
-                                        <p class="price_text">Price <span style="color: #262626;">$ 30</span></p>
-                                        <div class="tshirt_img"><img
-                                                src="{{ asset('asset/images/women-clothes-img.png') }}"></div>
-                                        <div class="btn_main">
-                                            <div class="buy_bt"><a href="#">Buy Now</a></div>
-                                            <div class="seemore_bt"><a href="#">See More</a></div>
-                                        </div>
+    @forelse (\App\Models\Category::take(10)->get()   as $Category)
+        @php
+            $products = \App\Models\Product::select(
+                'id',
+                'product_name',
+                'product_price',
+                'image',
+                'discount_percentage',
+                'category_id',
+            )
+                ->where('product_status', 1)
+                ->where('category_id', $Category->id)
+                ->orderByDesc('id')
+                ->get();
+
+            $groupedProducts = [];
+            $currentGroup = [];
+
+            foreach ($products as $product) {
+                $currentGroup[] = $product;
+
+                if (count($currentGroup) === 3) {
+                    $groupedProducts[] = $currentGroup;
+                    $currentGroup = [];
+                }
+            }
+
+            if (!empty($currentGroup)) {
+                $groupedProducts[] = $currentGroup;
+            }
+
+        @endphp
+        <div class="fashion_section">
+            <div id='{{ $Category->id }}' class="carousel product_main_slider slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    @forelse ($groupedProducts as $key=> $Products)
+                        <div class="carousel-item @if ($key == 0) active @endif">
+                            <div class="container">
+                                <h1 class="fashion_taital">{{ $Category->category_name }}</h1>
+                                <div class="fashion_section_2">
+                                    <div class="row">
+                                        @forelse ($Products as $Product)
+                                            <div class="col-lg-4 col-sm-4">
+                                                <div class="box_main">
+                                                    <h4 class="shirt_text">{{ $Product['product_name'] }}</h4>
+                                                    <p class="price_text">
+                                                        {{ number_format($Product->product_price - $Product->product_price * ($Product->discount_percentage / 100), 2) }}
+                                                        <del style="color: #262626;">{{ $Product['product_price'] }}</del>
+                                                    </p>
+                                                    @php
+                                                        $images = is_array($Product->image)
+                                                            ? $Product->image
+                                                            : [$Product->image];
+
+                                                        $firstImage = str_starts_with($images[0], 'Product/')
+                                                            ? url('storage', $images[0])
+                                                            : $images[0];
+                                                    @endphp
+                                                    <div class="electronic_img"><img src="{{ $firstImage }}">
+                                                    </div>
+                                                    <div class="btn_main">
+                                                        <div class="buy_bt"><a href="#">{{ __('page.Buy Now') }}</a>
+                                                        </div>
+                                                        <div class="seemore_bt"><a
+                                                                href="#">{{ __('page.See More') }}</a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        @empty
+                                        @endforelse
+
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    @empty
+                    @endforelse
                 </div>
-                <div class="carousel-item">
-                    <div class="container">
-                        <h1 class="fashion_taital">Man & Woman Fashion</h1>
-                        <div class="fashion_section_2">
-                            <div class="row">
-                                <div class="col-lg-4 col-sm-4">
-                                    <div class="box_main">
-                                        <h4 class="shirt_text">Man T -shirt</h4>
-                                        <p class="price_text">Price <span style="color: #262626;">$ 30</span></p>
-                                        <div class="tshirt_img"><img src="{{ asset('asset/images/tshirt-img.png') }}"></div>
-                                        <div class="btn_main">
-                                            <div class="buy_bt"><a href="#">Buy Now</a></div>
-                                            <div class="seemore_bt"><a href="#">See More</a></div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-4 col-sm-4">
-                                    <div class="box_main">
-                                        <h4 class="shirt_text">Man -shirt</h4>
-                                        <p class="price_text">Price <span style="color: #262626;">$ 30</span></p>
-                                        <div class="tshirt_img"><img
-                                                src="{{ asset('asset/images/dress-shirt-img.png') }}"></div>
-                                        <div class="btn_main">
-                                            <div class="buy_bt"><a href="#">Buy Now</a></div>
-                                            <div class="seemore_bt"><a href="#">See More</a></div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-4 col-sm-4">
-                                    <div class="box_main">
-                                        <h4 class="shirt_text">Woman Scart</h4>
-                                        <p class="price_text">Price <span style="color: #262626;">$ 30</span></p>
-                                        <div class="tshirt_img"><img
-                                                src="{{ asset('asset/images/women-clothes-img.png') }}"></div>
-                                        <div class="btn_main">
-                                            <div class="buy_bt"><a href="#">Buy Now</a></div>
-                                            <div class="seemore_bt"><a href="#">See More</a></div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <a class="carousel-control-prev" href="#{{ $Category->id }}" role="button" data-slide="prev">
+                    <i class="fa fa-angle-left"></i>
+                </a>
+                <a class="carousel-control-next" href="#{{ $Category->id }}" role="button" data-slide="next">
+                    <i class="fa fa-angle-right"></i>
+                </a>
             </div>
-            <a class="carousel-control-prev" href="#main_slider" role="button" data-slide="prev">
-                <i class="fa fa-angle-left"></i>
-            </a>
-            <a class="carousel-control-next" href="#main_slider" role="button" data-slide="next">
-                <i class="fa fa-angle-right"></i>
-            </a>
         </div>
-    </div>
+    @empty
+        <a class="dropdown-item" href="#">{{ trans('layout.There are no sections') }} </a>
+    @endforelse
 @endsection
